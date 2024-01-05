5bef3402b01ed1fb3967b6ae7040029ef36fcd5f
MikusR <mikus.rakis@gmail.com>
2024-01-05 12:36:00 +0200

    simplify

--- database/migrations/2023_12_22_112535_create_accounts_table.php
+++ database/migrations/2023_12_22_112535_create_accounts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->integer('user_id');
             $table->string('type');
             $table->string('currency');
